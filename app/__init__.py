edfe0e5d150bad76527603d278297ddd8a93b751
msm1998 <pc.pramukh99@gmail.com>
2020-07-26 11:56:47 +0530

    upload issue fixed

--- app/__init__.py
+++ app/__init__.py
@@ -5,5 +5,5 @@ MYDIR = os.path.dirname(__file__)
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['UPLOAD_FOLDER'] = '/static/temp'
+app.config['UPLOAD_FOLDER'] = 'static/temp'
 from app import routes 
